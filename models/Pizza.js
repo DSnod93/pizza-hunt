b42c5591deaa2580b18ebe9256cd1b2f28fc5e34
DSnoddy <douglassnoddyjr@gmail.com>
2021-04-05 22:41:43 -0800

    user can leave a comment

--- models/Pizza.js
+++ models/Pizza.js
@@ -8,24 +8,51 @@ The pizza's toppings
 */
 
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // set ID to false because mongo will add in this ID for the comments
+        id: false
+    }
+);
+// get total count of comments and replies on retrieval
+// reduce method to tally up the total amount of replies with each comment
+// reduce takes two parameters, an accumulator and a currentValue
+// accumulator is total
+// currentValue is comment
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
 });
+
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
